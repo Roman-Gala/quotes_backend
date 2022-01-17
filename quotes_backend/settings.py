0139a72684ac96cddab3183303ab5d443b8ca8b7
Roman Gala <92608673+Roman-Gala@users.noreply.github.com>
2022-01-17 17:36:48 +0100

    Removed secre_key

--- quotes_backend/settings.py
+++ quotes_backend/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ea&eooqhb7hp*0$jcjnsd0lg(m71a^n0ccdcp^ay(no#zt&ovd'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
